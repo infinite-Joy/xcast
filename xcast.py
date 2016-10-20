6695a78cf61d1e7a657e53200bd42876b4c66d9d
Gabor Szabo <gabor@szabgab.com>
2016-10-20 13:08:02 +0300

    code to try to collect data from the RSS feed of the floss-weekly

--- xcast.py
+++ xcast.py
@@ -1,25 +1,85 @@
 import argparse
+from datetime import datetime
 import glob
 import json
 import os
+import sys
 import re
 import csv
 from jinja2 import Environment, PackageLoader
 
-def main():
+if sys.version_info.major < 3:
+    print("This code requires Python 3.")
+    print("This is {}".format(sys.version))
+    exit()
 
-    with open('data/sources.json') as fh:
-        sources = json.load(fh)
+
+def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--list', help = 'List sources', action='store_true')
     parser.add_argument('--html', help = 'Generate HTML', action='store_true')
+    parser.add_argument('--check', help = 'Check the RSS feed', action='store_true')
+    parser.add_argument('--source', help = 'Check the RSS feed of given source')
     args = parser.parse_args()
 
+
+    with open('data/sources.json') as fh:
+        sources = json.load(fh)
     
     if args.list:
         for s in sources:
             print('{:20} {}'.format(s.get('name', ''), s.get('title', '')))
+    elif args.source:
+        source = list(filter(lambda x: x['name'] == args.source, sources))
+        if not source:
+            exit("'{}' is not one of the sources".format(args.source))
+        #print(source[0])
+        
+        import feedparser
+        d = feedparser.parse(source[0]['feed'])
+        for e in d.entries:
+            if args.source == 'floss-weekly':
+                data = {}
+                full_title = e['title']
+                published = e['published']
+                date = datetime.strptime(published, '%a, %d %b %Y %H:%M:%S %z')  # Tue, 18 Oct 2016 10:33:51 -0700
+                #comments  http://twit.tv/floss/408
+                #print(e['comments'])
+                #m = re.search(r'\d+$', e['comments'])
+                m = re.search(r'FLOSS Weekly (\d+):\s*(.*)', full_title)
+                ep = None
+                if m:
+                    data['ep'] = m.group(1)
+                    data['title'] = m.group(2)
+                    data['permalink'] = 'https://twit.tv/shows/floss-weekly/episodes/' + data['ep']
+                    data['date'] = date.strftime('%Y-%m-%d')
+                    data['guests'] = {}
+                    data['hosts'] = {}
+            else:
+                exit("Cannot handle this feed")
+            print(data)
+            print('---')
+            #exit()
+ 
+        #for k in d.entries[0].keys():
+        #    print("{}  {}\n".format(k, d.entries[0][k]))
+
+    elif args.check:
+        import feedparser
+        for s in sources:
+            if 'feed' in s:
+                print(s['feed'])
+                d = feedparser.parse(s['feed'])
+                print(d['feed']['title'])
+                print(d.entries[0].title)
+                print(d.entries[0].link)
+                print(d.entries[0])
+                print()
+                exit()
+
+            #else:
+            #    print('No feed for {}'.format(s['name']))
     elif args.html:
         episodes = read_episodes(sources)
         people = read_people()
