e8414ea847776ab7c9315a34c20ca85fba3822c1
Gabor Szabo <gabor@szabgab.com>
2016-10-23 16:22:24 +0300

    enable JavaScript based search

--- html/js/xcast.js
+++ html/js/xcast.js
@@ -1,11 +1,34 @@
 $(document).ready(function(){
-	//$('#q').css('display', 'inline');
-	$('#search').css('display', 'inline');
-    //console.log('Hello World');
-    $('#search').submit(function(e) {
-		//alert('here');
-		console.log('submit');
-		e.preventDefault();
-		//return false;
-	});
+    var availableTags = [];
+    var mapping = {}
+
+    $.getJSON("/search.json", function( result ) {
+        //console.log(result);
+        //console.log(result.length)
+        mapping = result;
+        for (var key in result) {
+            if (result.hasOwnProperty(key)) {
+                availableTags.push(key);
+            }
+        }
+        availableTags.sort();
+        //console.log(availableTags);
+
+    //    $('#q').css('display', 'inline');
+        $('#search').css('display', 'inline');
+        $( "#q" ).autocomplete({
+          source: availableTags
+        });
+        $('#search').submit(function(e) {
+            console.log('submit');
+            var term = $('#q').val();
+            e.preventDefault();
+            if (mapping[term]) { 
+                window.location.href = mapping[term];
+            }
+        });
+
+    });
+
 });
+// vim: expandtab
